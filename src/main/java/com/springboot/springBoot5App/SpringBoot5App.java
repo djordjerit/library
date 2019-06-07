f998f0c3243f08f45930a078ec72dd0d08f801f0
djordjeradivojevicfarm <djordjeradivojevicfarm@gmail.com>
2019-06-07 16:45:47 +0200

    Setting .gitignore file and starting project set up

---
+++ src/main/java/com/springboot/springBoot5App/SpringBoot5App.java
@@ -0,0 +1,12 @@
+package com.springboot.springBoot5App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot5App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot5App.class,args);
+    }
+}
